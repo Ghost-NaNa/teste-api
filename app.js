f1a6b6a4e72fde1e6690c36cd7b9a348ebd5e84d
Nathan <nathan.oltavares@gmail.com>
2026-05-06 21:13:21 -0300

    Update response message in Express API

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Arthur é legal');
+  res.send("Aprendi a upar API'S no vercel!");
 });
 
 app.listen(PORT, () => {
